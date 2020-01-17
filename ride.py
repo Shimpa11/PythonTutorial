aee4516d6ced1b0a7ca238def5b15e19fc490b05
Shimpa <er.shimpa@gmail.com>
2020-01-17 10:43:24 +0530

    Initial commit

---
+++ ride.py
@@ -0,0 +1,23 @@
+people={
+    1:{"name":"John","age":25},
+    2: {"name": "Jane", "age": 30},
+}
+print(people)
+print(people[1])
+
+print(people[1]['name'])
+
+print(people[1]['age'])
+
+
+ride_estimate= {
+      "category": "auto",
+      "distance": 16.9,
+      "travel_time_in_minutes": 93,
+      "amount_min": 227,
+      "amount_max": 249,
+      "booking_fee": 30,
+
+}
+print(ride_estimate)
+print(ride_estimate["distance"])
