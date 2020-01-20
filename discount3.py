ff4503e842c9d28006cf71a21a9140ca0467a9e9
Shimpa <er.shimpa@gmail.com>
2020-01-20 21:06:05 +0530

    new discount file

---
+++ discount3.py
@@ -0,0 +1,26 @@
+amount = int(input("Enter the amount:"))
+code=input("Enter Promo Code:")
+
+if amount>200:
+    if code=="zomato":
+        amount=amount-0.4*amount
+        print("pay", amount)
+    else:
+        print("try code zomato to get 40% OFF")
+
+elif amount>100:
+    if code == "JUMBO":
+        discount = 0.5 * amount
+        if discount > 150:
+            amount = amount - 150
+        else:
+            amount = amount - discount
+        print(">> JUMBO Applied Successfully !! 50% OFF upto 150")
+        print(">> Please Pay: \u20b9", amount)
+    else:
+        print(">> Try using JUMBO to get 50% OFF")
+else:
+    print(">> Please Pay: \u20b9", amount)
+
+
+
